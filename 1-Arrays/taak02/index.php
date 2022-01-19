d478cb22a6ea5432cb14ffd8e1f7ef455c18622b
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-19 12:15:47 +0100

    Opdracht 2 Lorenzo

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,10 @@
+<?php
+$persoon = array(
+    "voornaam"=>" Willem Alexander",
+    "achternaam"=>" van oranje",
+    "leeftijd"=>" 53",
+    "opleiding"=>" Software ontwikkeling"
+);
+echo $persoon ["voornaam"]. $persoon ["achternaam"]. " is". $persoon ["leeftijd"]. "jaar oud en is student aan het ROC van Amsterdam aan de opleiding". $persoon ["opleiding"];
+?>
+
