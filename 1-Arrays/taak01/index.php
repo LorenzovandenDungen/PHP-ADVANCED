829169763e93fda387839a3e54924ce6bdf14ce3
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-19 11:52:58 +0100

    opdracht 1 Lorenzo

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,8 @@
+<?php
+$student = array(
+    "voornaam"=>" Lorenzo",
+    "achternaam"=> "van den Dungen",
+    "klas"=>" 1A"
+);
+echo "Deze student heet". $student["voornaam"]. " en zit in klas". $student["klas"];
+?>
