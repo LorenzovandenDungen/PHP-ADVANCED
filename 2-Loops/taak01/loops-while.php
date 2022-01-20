59648f8863e23e104487355b59da48d99a5126a5
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-20 10:57:32 +0100

    loops-while

--- 2-Loops/taak01/loops-while.php
+++ 2-Loops/taak01/loops-while.php
@@ -3,14 +3,13 @@ $leeftijd = 0;
 
 echo '<ul>';
 
-while($leeftijd < 18){
+while($leeftijd <= 18){
+    if($leeftijd == 18){
+        echo "<li> ik ben: $leeftijd jaar, dus ik mag stemmen! </li>";
+        break;
+    }
     echo "<li> Ik ben: $leeftijd jaar, dus ik mag nog niet stemmen </li>";
     $leeftijd++;
-}
-
-
-if($leeftijd = 18){
-    echo "<li> ik ben: $leeftijd jaar, dus ik mag stemmen! </li>";
 }
 
 echo '</ul>';
