6f1a29ff1146a48f6599bcff681488cfa863f3e3
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-20 10:51:13 +0100

    loops-for

--- 2-Loops/taak02/loops-for.php
+++ 2-Loops/taak02/loops-for.php
@@ -3,13 +3,14 @@ $x = 0;
 
 echo "<ul>";
 
-for ($x = 0; $x <= 17; $x++){
+for ($x = 0; $x <= 18; $x++){
+    if ($x == 18){
+        echo "<li>Ik ben $x jaar oud, dus ik heb stemrecht!</li>"; 
+        break;
+    }
     echo "<li>ik ben $x jaar, dus ik mag nog niet stemmen</li>";
 }
 
-if($x = 18){
-    echo "<li>Ik ben $x jaar oud, dus ik heb stemrecht!</li>";
-}
 
 echo "</ul>";
 ?>
