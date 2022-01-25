762f16852228c5df4ce67701e50882b897c75708
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-25 12:43:35 +0100

    loops-foreach-deel1

--- 2-Loops/taak03/loops-foreach-deel1.php
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -2,17 +2,10 @@
 
 echo "<ul>";
 
-<<<<<<< HEAD
 $dagen = array("maandag", "dinsdag", "woendag", "donderdag", "vrijdag", "zaterdag", "zondag");
 
 foreach ($dagen as $s){
     echo $s."<li/>";
-=======
-$dagen = array('maandag', 'dinsdag', 'woendag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag');
-
-foreach ($dagen as $s){
-    echo $s."<li><br/>";
->>>>>>> 886b4a15e1ee2ea285253510fde38a4494a3b69e
 }
 
 echo "</ul>";
