2ae07386e24717f53c0eadb40f4ba4ca2b3aba10
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-01-25 13:45:35 +0100

    loops-foreach-deel2.php

--- 2-Loops/taak04/loops-foreach-deel3.php
+++ 2-Loops/taak04/loops-foreach-deel3.php
@@ -1,9 +1,36 @@
-<?php
-$student = array(
-    "voornaam" => "Jan",
-    "achternaam" => "Janssen",
-    "klas" => "9A",
-    "leeftijd" => 17,
-    "woonplaats" => "Amstelveen"
-)
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div>Ik zit in klas</div>
+        <div>
+
+            <?php
+
+                $selected = "8A";
+                $klassen = array('8A', '8B', '8C', '8D', '8E', '9A', '9B', '9C', '9D', '9E');
+
+                echo "<select>";
+                foreach($klassen as $klas){
+                    if($selected == $klas){
+                        echo "<option selected='selected' value='$klas'>$klas</option>";
+                    }
+                    else {
+                        echo "<option value='$klas'>$klas</option>";
+                    }
+                }
+                echo "</select>"
+            ?>
+        </div>
+    </div>    
+    <input type="submit">
+</body>
+</html>
+
+
