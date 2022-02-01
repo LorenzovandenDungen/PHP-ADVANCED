40a9ad7153dd309542e1b0bf586a0e538268a9fd
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-02-01 15:38:20 +0100

    Nog niet volledig af

--- 3-Forms/taak01/mijngegevens.php
+++ 3-Forms/taak01/mijngegevens.php
@@ -13,4 +13,8 @@ echo "plaatsnaam:". $_POST["plaatsnaam"];
 echo "<br>";
 echo "favoriete_muziekband:". $_POST["favoriete_muziekband"];
 echo "<br>";
+$geslacht = $_POST['geslacht'];
+echo "geslacht:" .$geslacht;
+echo "<br/>";
+echo "kleuren:". $_POST["kleuren"];
 ?>
