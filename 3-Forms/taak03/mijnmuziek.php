c1dfc14f9407ab4be271ab940ec4f210edf6de22
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-02-03 12:17:49 +0100

    GET

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form name="" action="" method="get"><br/>
+    Favorite muziekartiest<input type="text" name="artiest" placeholder="vul in"><br/>
+    titel<input type="text" name="titel" placeholder="vul in"><br/>
+    jaar van uitkomst<input type="text" name="jaar_van_uitkomst" placeholder="vul in"><br/>
+    Muziek soort<input type="text" name="muzieksoort" placeholder="vul in"><br/>
+    <input type="submit" name="submit" value="verstuur"><br/>
+    Dit is mijn link http://localhost/blok%204/PHP-ADVANCED/3-Forms/taak03/mijnmuziek.php?artiest=Eminem&titel=lose+yourself&jaar_van_uitkomst=2002&muzieksoort=rap&submit=verstuur
+</form>
+</body>
+</html>
