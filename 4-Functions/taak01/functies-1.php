395d16b0e4ee1add0934eb790e31da16ba00a3a7
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-02-03 12:41:41 +0100

    klaar

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function schrijfKleur($kleur){
+            echo "<font color=$kleur>Deze tekst is in de kleur $kleur";
+        }
+        schrijfKleur("red");
+        echo "<br>";
+        schrijfKleur("orange");
+        echo "<br>";
+        schrijfKleur("yellow");
+        echo "<br>";
+        schrijfKleur("black");
+    ?>
+</body>
+</html>
