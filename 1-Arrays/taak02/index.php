cd3b7c494db9decfe25966acaf8935ab0ba17d64
LorenzovandenDungen <90601094+LorenzovandenDungen@users.noreply.github.com>
2022-02-11 15:42:50 +0100

    Update index.php

--- 1-Arrays/taak02/index.php
+++ 1-Arrays/taak02/index.php
@@ -1,10 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $persoon = array(
-    "voornaam"=>" Willem Alexander",
+    "voornaam"=>"Willem Alexander" ,
     "achternaam"=>" van oranje",
     "leeftijd"=>" 53",
     "opleiding"=>" Software ontwikkeling"
 );
-echo $persoon ["voornaam"]. $persoon ["achternaam"]. " is". $persoon ["leeftijd"]. "jaar oud en is student aan het ROC van Amsterdam aan de opleiding". $persoon ["opleiding"];
+echo $persoon ["voornaam"]. $persoon ["achternaam"]. " is". $persoon ["leeftijd"]. " jaar oud en is student aan het ROC van Amsterdam aan de opleiding". $persoon ["opleiding"];
 ?>
 
+
+</body>
+</html>
