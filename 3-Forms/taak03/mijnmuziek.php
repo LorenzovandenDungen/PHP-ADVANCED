6f7e52a6609a251f76e2eeef2e42ca737269a906
LorenzovandenDungen <Lorenzo.vandendungen@student.rocva.nl>
2022-02-11 09:48:26 +0100

    verbeterd

--- 3-Forms/taak03/mijnmuziek.php
+++ 3-Forms/taak03/mijnmuziek.php
@@ -7,13 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-<form name="" action="" method="get"><br/>
-    Favorite muziekartiest<input type="text" name="artiest" placeholder="vul in"><br/>
-    titel<input type="text" name="titel" placeholder="vul in"><br/>
-    jaar van uitkomst<input type="text" name="jaar_van_uitkomst" placeholder="vul in"><br/>
-    Muziek soort<input type="text" name="muzieksoort" placeholder="vul in"><br/>
-    <input type="submit" name="submit" value="verstuur"><br/>
-    Dit is mijn link http://localhost/blok%204/PHP-ADVANCED/3-Forms/taak03/mijnmuziek.php?artiest=Eminem&titel=lose+yourself&jaar_van_uitkomst=2002&muzieksoort=rap&submit=verstuur
-</form>
+<form name="mijnmuziek" action="mijngegevens.php" method="get">
+         Artiest : <input type="text" name="artiest"><br>
+         Titel : <input type="text" name="titel"><br>
+         Jaar van uitkomst : <input type="text" name="jaarvanuitkomst"><br>
+         Muzieksoort : <input type="text" name="muzieksoort"><br>
+         <input type="submit" name="submit" value="verstuur">
+    </form>
 </body>
 </html>
